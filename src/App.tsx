7a9e1fbfc240bd6093bf0d5785d06a69ddaf06c3
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-09-04 09:39:34 +0630

    resove conflic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
-import { MuiButtonGroup } from './components/MuiButtonGroup';
-=======
 import { MuiButton } from './components/MuiButton';
->>>>>>> origin/master
 // import { MuiTypography } from './components/MuiTypography';
 
 function App() {
   return (
     <div className='App'>
       {/* <MuiTypography /> */}
-<<<<<<< HEAD
-      <MuiButtonGroup />
-=======
       <MuiButton />
->>>>>>> origin/master
     </div>
   );
 }
