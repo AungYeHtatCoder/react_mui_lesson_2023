ab705e181a845e8ba1b86463e2f999b2a08cc38b
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-09-04 08:26:43 +0630

    first_lesson typography by amk

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import { Typography } from '@mui/material';
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Heading 1</Typography>
+      <Typography variant='h2'>Heading 2</Typography>
+      <Typography variant='h3'>Heading 3</Typography>
+      <Typography variant='h4'>Heading 4</Typography>
+      <Typography variant='h5'>Heading 5</Typography>
+      <Typography variant='h6'>Heading 6</Typography>
+      <Typography variant='subtitle1'>Subtitle 1</Typography>
+      <Typography variant='subtitle2'>Subtitle 2</Typography>
+      <Typography variant='body1'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate
+        pariatur sed expedita totam molestias, quia laboriosam impedit soluta
+        libero excepturi eius est quo, earum voluptas necessitatibus consequatur
+        amet veritatis inventore.
+      </Typography>
+      <Typography variant='body2'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        libero quia ratione mollitia repellendus quidem voluptatum illum eum
+        voluptatem sapiente alias animi, soluta, ut blanditiis illo. Corrupti
+        iusto quos officiis.
+      </Typography>
+      <Typography variant='button'>Button</Typography>
+      <Typography variant='caption'>Caption</Typography>
+      <Typography variant='overline'>Overline</Typography>
+    </div>
+  );
+};
