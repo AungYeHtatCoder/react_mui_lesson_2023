75986cb1160e9ef7411720db5eae9901aa4c1b8c
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-09-04 10:18:22 +0630

    toggle button by amk

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,6 +1,27 @@
-import { Stack, Button, IconButton } from '@mui/material';
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+} from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import { useState } from 'react';
 export const MuiButton = () => {
+  const [formats, setFormats] = useState<string | null>(null);
+  console.log({
+    formats,
+  });
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string | null
+  ) => {
+    setFormats(updatedFormats);
+  };
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction={'row'}>
@@ -112,6 +133,33 @@ export const MuiButton = () => {
         >
           Send
         </Button>
+      </Stack>
+      <Stack spacing={2} direction={'row'}>
+        <Button variant='contained' color='primary' disabled>
+          Send
+        </Button>
+      </Stack>
+
+      <Stack direction={'row'}>
+        <ToggleButtonGroup
+          aria-label='text formatting'
+          value={formats}
+          onChange={handleFormatChange}
+          size='small'
+          color='success'
+          orientation='vertical'
+          exclusive
+        >
+          <ToggleButton value={'bold'} aria-label='bold'>
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value={'italic'} aria-label='italic'>
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value={'underline'} aria-label='underline'>
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
